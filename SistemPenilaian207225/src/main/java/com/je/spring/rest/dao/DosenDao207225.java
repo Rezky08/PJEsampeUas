1a91c75786415854e4d0bc934821d72eee0cf1fb
rezky setiawan <rezky.setiawan85@gmail.com>
2020-04-20 07:50:03 +0700

    add all files

---
+++ SistemPenilaian207225/src/main/java/com/je/spring/rest/dao/DosenDao207225.java
@@ -0,0 +1,19 @@
+/*
+ Kelompok 1 = Rezky(20)-Ravi(72)-Alfan(25) 
+ */
+package com.je.spring.rest.dao;
+
+import com.je.spring.rest.model.Dosen207225;
+import java.util.List;
+
+public interface DosenDao207225 {
+
+    public List<Dosen207225> getAll();
+
+    public Dosen207225 getById(int id207225);
+    public void insert(Dosen207225 dosen207225);
+    public void update(Dosen207225 dosen207225);
+    public void delete(Dosen207225 dosen207225);
+
+    public long count();
+}
---
+++ SistemPenilaian207225/src/main/java/com/je/spring/rest/model/Dosen207225.java
@@ -0,0 +1,57 @@
+/*
+ Kelompok 1 = Rezky(20)-Ravi(72)-Alfan(25) 
+ */
+package com.je.spring.rest.model;
+
+public class Dosen207225 {
+
+    private int id207225;
+    private String nama207225;
+    private String alamat207225;
+    private String jenkel207225;
+    private String nohp207225;
+
+    public int getId207225() {
+        return id207225;
+    }
+
+    public void setId207225(int id207225) {
+        this.id207225 = id207225;
+    }
+
+    public String getNama207225() {
+        return nama207225;
+    }
+
+    public void setNama207225(String nama207225) {
+        this.nama207225 = nama207225;
+    }
+
+    public String getAlamat207225() {
+        return alamat207225;
+    }
+
+    public void setAlamat207225(String alamat207225) {
+        this.alamat207225 = alamat207225;
+    }
+
+    public String getJenkel207225() {
+        return jenkel207225;
+    }
+
+    public void setJenkel207225(String jenkel207225) {
+        this.jenkel207225 = jenkel207225;
+    }
+
+    public String getNohp207225() {
+        return nohp207225;
+    }
+
+    public void setNohp207225(String nohp207225) {
+        this.nohp207225 = nohp207225;
+    }
+
+    
+    
+
+}
